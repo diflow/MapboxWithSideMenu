7ceb5992503a20e589e8301be2bbcef5074dae2e
diflow <dvornyk.ivan@gmail.com>
2021-02-12 11:16:50 +0200

    added function showUserLocation

--- MapboxWithSideMenu/ViewController.swift
+++ MapboxWithSideMenu/ViewController.swift
@@ -6,14 +6,76 @@
 //
 
 import UIKit
+import Mapbox
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MGLMapViewDelegate, CLLocationManagerDelegate {
+    
+    var mapView: MGLMapView!
+    var locationManager = CLLocationManager()
+    let userLocationButton = UIButton()
+    var defaultPosition = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setupConstraints()
+        userLocationButton.addTarget(self, action: #selector(showUserLocation), for: .touchUpInside)
     }
+    
+    @objc func showUserLocation() {
+        guard let userLocation = mapView.userLocation?.coordinate else { return }
+        mapView.setCenter(userLocation, zoomLevel: 15, animated: true)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        determineMyCurrentLocation()
+    }
+    
+    func determineMyCurrentLocation() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation = locations[0] as CLLocation
+        
+        if !defaultPosition {
+            mapView.setCenter(userLocation.coordinate, zoomLevel: 15, animated: false)
+            defaultPosition = true
+        }
+        manager.stopUpdatingLocation()
+    }
+}
 
-
+extension ViewController {
+    
+    private func setupConstraints() {
+        
+        mapView = MGLMapView(frame: view.bounds)
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        
+        userLocationButton.setImage(#imageLiteral(resourceName: "refresh"), for:.normal)
+        
+        userLocationButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(mapView)
+        view.addSubview(userLocationButton)
+        
+        NSLayoutConstraint.activate([
+            userLocationButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -200),
+            userLocationButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -12),
+            userLocationButton.heightAnchor.constraint(equalToConstant: 40),
+            userLocationButton.widthAnchor.constraint(equalToConstant: 40)
+        ])
+    }
 }
 
