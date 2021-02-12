ba6e9e8b30d4591aa4c665bad1313c637be8cc70
diflow <dvornyk.ivan@gmail.com>
2021-02-12 11:28:47 +0200

    sideMenuButton added

--- MapboxWithSideMenu/ViewController.swift
+++ MapboxWithSideMenu/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController, MGLMapViewDelegate, CLLocationManagerDel
     
     var mapView: MGLMapView!
     var locationManager = CLLocationManager()
+    let sideMenuButton = UIButton()
     let userLocationButton = UIButton()
     var defaultPosition = false
 
@@ -20,11 +21,16 @@ class ViewController: UIViewController, MGLMapViewDelegate, CLLocationManagerDel
 
         setupConstraints()
         userLocationButton.addTarget(self, action: #selector(showUserLocation), for: .touchUpInside)
+        sideMenuButton.addTarget(self, action: #selector(showSideMenu), for: .touchUpInside)
     }
     
     @objc func showUserLocation() {
         guard let userLocation = mapView.userLocation?.coordinate else { return }
         mapView.setCenter(userLocation, zoomLevel: 15, animated: true)
+    }
+    
+    @objc func showSideMenu() {
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -64,17 +70,27 @@ extension ViewController {
         mapView.showsUserLocation = true
         
         userLocationButton.setImage(#imageLiteral(resourceName: "refresh"), for:.normal)
+        sideMenuButton.setImage(#imageLiteral(resourceName: "menu"), for:.normal)
         
         userLocationButton.translatesAutoresizingMaskIntoConstraints = false
+        sideMenuButton.translatesAutoresizingMaskIntoConstraints = false
         
         view.addSubview(mapView)
         view.addSubview(userLocationButton)
+        view.addSubview(sideMenuButton)
         
         NSLayoutConstraint.activate([
             userLocationButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -200),
             userLocationButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -12),
             userLocationButton.heightAnchor.constraint(equalToConstant: 40),
             userLocationButton.widthAnchor.constraint(equalToConstant: 40)
+        ])
+        
+        NSLayoutConstraint.activate([
+            sideMenuButton.topAnchor.constraint(equalTo: mapView.topAnchor, constant: 50),
+            sideMenuButton.leadingAnchor.constraint(equalTo: mapView.leadingAnchor, constant: 12),
+            sideMenuButton.heightAnchor.constraint(equalToConstant: 40),
+            sideMenuButton.widthAnchor.constraint(equalToConstant: 40)
         ])
     }
 }
